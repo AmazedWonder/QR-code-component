0bdd096d8c9aaed7e6f189f146f73b757e50b95e
Wonder <111242876+AmazedWonder@users.noreply.github.com>
2025-05-30 14:37:32 +0100

    Update QRCodeCard.tsx

--- components/QRCodeCard.tsx
+++ components/QRCodeCard.tsx
@@ -5,26 +5,27 @@ import Image from 'next/image';
 
 export default function QRCodeCard() {
     return (
-        <div className="max-w-xs mx-auto bg-white p-4 rounded-2xl shadow-lg flex flex-col items-center gap-6 text-center dark:bg-[#1c1c1c] dark:text-white">
+        <div className="max-w-xs mx-auto bg-white p-4 rounded-2xl text-center shadow-lg transition transform hover:scale-105 hover:shadow-2xl duration-300">
+            {/* flex flex-col items-center gap-6 text-center dark:bg-[#1c1c1c] dark:text-white */}
             <Image
                 src="/images/image-qr-code.png"
                 alt='QR Code'
                 width={288}
                 height={288}
-                className='rounded-xl'
+                className='rounded-xl mb-4 w-full shadow-lg transition-transform duration-300 hover:scale-105'
             />
 
-            <div className="">
-                <h1 className="">
+            <div className="px-2">
+                <h1 className="text-lg font-bold text-slate-800 mb-2">
                     Improve your front-end skills by building projects
                 </h1>
-                <p className="">
+                <p className="text-sm text-slate-500 mb-4">
                     Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.
                 </p>
             </div>
 
-            <div className="">
-                Challenge by{' '}
+            <div className="text-xs text-center text-slate-400 mt-2">
+                Challenge by {' '}
                 <a
                     className=""
                     href="https://www.frontendmentor.io?ref=challenge"
